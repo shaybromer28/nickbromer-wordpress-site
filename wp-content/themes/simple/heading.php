9697cd6e4417568e6279a3c6ecef7803d6bdb0de
Shay Bromer <shaybromer@gmail.com>
2016-12-11 16:48:00 -0500

    UI Tweaks

--- wp-content/themes/simple/heading.php
+++ wp-content/themes/simple/heading.php
@@ -1,6 +1,4 @@
 <div class="heading">
 <h2><?php bloginfo ('name'); ?></h2>
-<p class="tagline"><i>Custom Product Invention.</i></p>
-<p class="tagline"><i>Patentable Technology.</i></p>
-<p class="tagline"><i>Creative Insights.</i></p>
+<p class="tagline"><i><?php bloginfo ('description') ?></i></p>
 </div>
